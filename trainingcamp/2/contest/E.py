a10e6067c1ae9ec5196173d34abfdf979a2597ee
jero98772 <jero98772@protonmail.com>
2022-07-09 12:17:47 -0500

    add training camp

---
+++ trainingcamp/2/contest/E.py
@@ -0,0 +1,5 @@
+input()
+name=input()
+pokes=["vaporeon", "jolteon", "flareon", "espeon", "umbreon", "leafeon", "glaceon", "sylveon"]
+for  
+
---
+++ trainingcamp/template.cpp
@@ -0,0 +1,5 @@
+#define p(x) cout<<x<<endl;
+#include <bits/stdc++.h>
+int main(){
+  return 0;
+}
