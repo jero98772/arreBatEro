4e26842e5d4f32bc246c173699833b6249d5d828
jero98772 <jero98772@protonmail.com>
2022-07-19 21:08:46 -0500

    update

--- cf/main.py
+++ cf/main.py
@@ -4,6 +4,8 @@ app=Flask(__name__)
 
 FILETEST="data/test.txt"
 CODE="data/code.py"
+#python a.py > t2 
+
 def wr(name,conent,mode):
 	with open(name,mode) as f:
 		f.write(conent)
@@ -20,7 +22,8 @@ def testcases():
 def index():
 	if request.methot=="POST":
 		code=request.from["code"]
-		wr()
+		wr(code)
+		run("python "+CODE+" > data/tests2.txt")
 	
 	return render_template("index.html")
 if __name__=="__main__":
