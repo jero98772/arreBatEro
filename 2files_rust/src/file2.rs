23c35e664e89789dc66f848a9d7b61776b913fd8
jero98772 <jero98772@gmail.com>
2024-03-12 11:44:34 -0500

    update

---
+++ 2files_rust/src/file2.rs
@@ -0,0 +1,10 @@
+pub mod functions {
+    pub fn some_function() {
+        // Function implementation
+        println!("Hello from another file!");
+    }
+   pub fn some_function2() {
+        // Function implementation
+        println!("Hello from another file and f2!");
+    }
+}
---
+++ 2files_rust/src/main.rs
@@ -0,0 +1,8 @@
+mod file2;
+
+use file2::functions::{some_function,some_function2};
+
+fn main() {
+    some_function();
+    some_function2();
+}
