833dbde35cfb8c3a38b3b2adaceedd72172cd3e3
dasjero98772 <jero98772@gmail.com>
2025-06-17 19:49:06 -0500

    update

---
+++ hackassembler_compiler/clojure_hackasembler_web/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure_hackasembler_web
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ hackassembler_compiler/clojure_hackasembler_web/test/clojure_hackasembler_web/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-hackasembler-web.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-hackasembler-web.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
