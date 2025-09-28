8ff21ed2077c7c32aa0c36318f5f350661fc13dc
jero98772 <jero98772@protonmail.com>
2025-09-28 09:40:56 -0500

    Update simplefold_test.py

--- simplefold_test.py
+++ simplefold_test.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# For licensing see accompanying LICENSE file.
-# Copyright (c) 2025 Apple Inc. Licensed under MIT License.
-#
-
 """
 Protein structure prediction with SimpleFold - Fixed for memory issues.
 """
